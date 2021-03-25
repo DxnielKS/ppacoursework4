08c225d339e19a667df5a5bf4b434e23d4b303f7
Moin Uddin <Lsfhv@protonmail.com>
2021-03-25 16:31:58 +0000

    Made it nicer

--- GUI.java
+++ GUI.java
@@ -10,10 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import javax.swing.*;
@@ -24,8 +21,22 @@ public class GUI extends Application
     @Override
     public void start(Stage stage)
     {
-
         BorderPane root = new BorderPane();
+        root = create(root);
+        Scene scene = new Scene(root, 800,500);
+        stage.setTitle("JavaFX Example");
+        stage.setScene(scene);
+
+        // Show the Stage (window)
+        stage.show();
+    }
+
+    /**
+     * Sets up the initial interface
+     * @param root
+     * @return
+     */
+    private BorderPane create(BorderPane root) {
         BorderPane top = new BorderPane();
         BorderPane bottom = new BorderPane();
 
@@ -44,10 +55,10 @@ public class GUI extends Application
         HBox topright = new HBox(10);
 
         ChoiceBox fromChoiceBox = new ChoiceBox();
-        fromChoiceBox.getItems().add("Choice 1");
+        fromChoiceBox.getItems().add("PRICERANGES");
 
         ChoiceBox toChoiceBox = new ChoiceBox();
-        toChoiceBox.getItems().add("Choice 1");
+        toChoiceBox.getItems().add("PREARRANGES");
 
         topright.getChildren().addAll(new Label("From:"), fromChoiceBox, new Label("To:"), toChoiceBox);
 
@@ -55,15 +66,23 @@ public class GUI extends Application
         top.setRight(topright);
 
         bottom.setPadding(new Insets(15, 20, 10, 10));
-
+        //rightArrow.setOnAction(this::buttonClick);
         root.setTop(top);
         root.setBottom(bottom);
-        Scene scene = new Scene(root, 800,500);
-        stage.setTitle("JavaFX Example");
-        stage.setScene(scene);
-
-        // Show the Stage (window)
-        stage.show();
+        root.setCenter(welcomePane());
+        return root;
     }
+
+    /**
+     * The welcome pane
+     * @return
+     */
+    private StackPane welcomePane() {
+        StackPane stackPane = new StackPane();
+        Label label = new Label("Welcome!, Select a price range above and then use arrows to navigate the different panes");
+        stackPane.getChildren().addAll(label);
+        return stackPane;
+    }
+
 
 }
